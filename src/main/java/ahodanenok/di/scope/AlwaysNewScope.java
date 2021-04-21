8de9d705ec959635bff9dcee21570b2c6adfc41a
Andrei Hodanenok <xoda91@gmail.com>
2021-04-21 23:23:34 +0300

    Improvements for AlwaysNewScope

--- src/main/java/ahodanenok/di/scope/AlwaysNewScope.java
+++ src/main/java/ahodanenok/di/scope/AlwaysNewScope.java
@@ -2,11 +2,15 @@ package ahodanenok.di.scope;
 
 import javax.inject.Provider;
 
+/**
+ * Instantiates object on every request
+ */
 public class AlwaysNewScope<T> implements Scope<T> {
 
     private static final AlwaysNewScope<?> INSTANCE = new AlwaysNewScope<>();
 
-    @SuppressWarnings("unchecked")
+    // method is a helper to avoid doing casts in client code
+    @SuppressWarnings("unchecked") // the scope doesn't care about type, returns what provider gave
     public static <T> AlwaysNewScope<T> getInstance() {
         return (AlwaysNewScope<T>) INSTANCE;
     }
